c6708c90243ceeb8af3dee422d353740d523f6f5
hpark95 <parkhs1995@gmail.com>
2019-04-11 21:16:18 -0400

    minor fixes

--- processing.py
+++ processing.py
@@ -17,17 +17,23 @@ def selectPoints(filename):
     img = Image.open(filename)
     plt.imshow(img)
 
-    # Take an user input for two pairs of two parallel lines
-    # In total, we will collect 4 points from the user
+    # Initialize a variable to take user inputs
     points = []
 
+    # Take an user input for two pairs of two parallel lines
+    # In total, we will collect 8 points from the user
     for num_lines in range(4):
+        # Take a point from the user and plot it
         points.append(plt.ginput(n = 1)[0])
         plt.scatter(points[num_lines * 2][0], points[num_lines * 2][1], c = 'black')
         plt.draw()
+
+        # Take another point from the user and plot it
         points.append(plt.ginput(n = 1)[0])
         plt.scatter(points[num_lines * 2 + 1][0], points[num_lines * 2 + 1][1], c = 'black')
         plt.draw()
+
+        # Plot the line passing the two input points
         plt.plot([points[num_lines * 2][0], points[num_lines * 2 + 1][0]], [points[num_lines * 2][1], points[num_lines * 2 + 1][1]], 'black', linewidth = 1.5)
 
     return points
